9cf8c44725d5a1a04f1a101da5be3cbd8eca5bc4
nguyenthanhan2121110226 <an2121110226@gmail.com>
2024-01-26 19:57:53 +0700

    first commit

--- APIJavaSpringBoot/src/main/java/com/example/example3/repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.example3.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.example3.entity.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-
-}
